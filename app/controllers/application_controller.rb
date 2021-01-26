ad35b41376f7e71954135a1bb4458991b8129b14
OccassionalDev <ericaschimmel01@gmail.com>
2021-01-25 21:19:12 -0500

    moved devise_for :users routes under a /api scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, :if => :devise_controller?
 
-    protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
