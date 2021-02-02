2d142d740552aab71b11ddf43e49c28f1c0ddeb1
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-02 16:16:40 -0500

    rubocop fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::API
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :email, :password, :password_confirmation, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in, :keys => [:email, :password, :remember_me])
-    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      :keys => [:username, :email, :password, :password_confirmation, :remember_me])
   end
 end
