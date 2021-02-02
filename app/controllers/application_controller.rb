606ce929a5b0da96ee0c06f1d263b6a465248476
OccassionalDev <ericaschimmel01@gmail.com>
2021-02-02 16:14:04 -0500

    updated syntax of devise configuration of permitted parameters in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :email, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, :keys => [:email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:username, :email, :password, :password_confirmation, :remember_me])
   end
 end
